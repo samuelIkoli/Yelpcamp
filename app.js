0075b17cd4fd47aa7444a04da468cc6b7ed86458
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2023-08-07 15:12:26 +0100

    support for vercel

--- app.js
+++ app.js
@@ -75,6 +75,13 @@ const sessionConfig = {
     }
 }
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(session(sessionConfig))
 app.use(flash());
 // app.use(helmet({ contentSecurityPolicy: false }))
